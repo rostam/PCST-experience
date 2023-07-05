106d8e48da7b5139719a6f6bfbd43e3e9748b4fb
Ali Rostami <rostami@axxessio.com>
2023-07-05 07:36:15 +0200

    kruskal_pcst to solve pcst

---
+++ kruskal_pcst.cpp
@@ -0,0 +1,113 @@
+#include <vector>
+#include <algorithm>
+#include <cassert>
+
+struct Edge {
+    int node1;
+    int node2;
+    double cost;
+};
+
+struct Node {
+    int id;
+    double prize;
+};
+
+// Function to find the parent of a node in the disjoint set
+int find(std::vector<int>& parent, int i) {
+    if (parent[i] == -1)
+        return i;
+    return find(parent, parent[i]);
+}
+
+// Function to union two sets
+void unionSet(std::vector<int>& parent, int i, int j) {
+    int i_set = find(parent, i);
+    int j_set = find(parent, j);
+    parent[i_set] = j_set;
+}
+
+std::vector<Edge> kruskalPCST(std::vector<Node>& nodes, std::vector<Edge>& edges) {
+    // Sort edges in increasing order of cost and decreasing order of prize-to-cost ratio
+    std::sort(edges.begin(), edges.end(), [&nodes](const Edge& a, const Edge& b) {
+        double ratioA = std::max(nodes[a.node1].prize, nodes[a.node2].prize) / a.cost;
+        double ratioB = std::max(nodes[b.node1].prize, nodes[b.node2].prize) / b.cost;
+        if (a.cost == b.cost) {
+            return ratioA > ratioB;
+        } else {
+            return a.cost < b.cost;
+        }
+    });
+
+    std::vector<int> parent(nodes.size(), -1);
+    std::vector<Edge> result;
+
+    for (const auto& edge : edges) {
+        int x = find(parent, edge.node1);
+        int y = find(parent, edge.node2);
+
+        // If including this edge does not form a cycle and the cost of the edge is less than the prize of either node
+        if (x != y && (edge.cost < nodes[edge.node1].prize || edge.cost < nodes[edge.node2].prize)) {
+            result.push_back(edge);
+            unionSet(parent, x, y);
+        }
+    }
+
+    return result;
+}
+
+
+double calculateCost(const std::vector<Edge>& tree) {
+    double cost = 0;
+    for (const auto& edge : tree) {
+        cost += edge.cost;
+    }
+    return cost;
+}
+
+double calculatePrize(const std::vector<Edge>& tree, const std::vector<Node>& nodes) {
+    double prize = 0;
+    std::vector<bool> included(nodes.size(), false);
+    for (const auto& edge : tree) {
+        if (!included[edge.node1]) {
+            prize += nodes[edge.node1].prize;
+            included[edge.node1] = true;
+        }
+        if (!included[edge.node2]) {
+            prize += nodes[edge.node2].prize;
+            included[edge.node2] = true;
+        }
+    }
+    return prize;
+}
+
+void test1() {
+    std::vector<Node> nodes = {{0, 1}, {1, 2}, {2, 3}};
+    std::vector<Edge> edges = {{0, 1, 1}, {1, 2, 2}};
+    std::vector<Edge> tree = kruskalPCST(nodes, edges);
+    assert(calculateCost(tree) <= 3);
+    assert(calculatePrize(tree, nodes) >= 6);
+}
+
+void test2() {
+    std::vector<Node> nodes = {{0, 1}, {1, 2}, {2, 3}, {3, 4}};
+    std::vector<Edge> edges = {{0, 1, 1}, {1, 2, 2}, {2, 3, 3}};
+    std::vector<Edge> tree = kruskalPCST(nodes, edges);
+    assert(calculateCost(tree) <= 6);
+    assert(calculatePrize(tree, nodes) >= 10);
+}
+
+void test3() {
+    std::vector<Node> nodes = {{0, 1}, {1, 2}, {2, 3}, {3, 4}, {4, 5}};
+    std::vector<Edge> edges = {{0, 1, 1}, {1, 2, 2}, {2, 3, 3}, {3, 4, 4}};
+    std::vector<Edge> tree = kruskalPCST(nodes, edges);
+    assert(calculateCost(tree) <= 10);
+    assert(calculatePrize(tree, nodes) >= 15);
+}
+
+int main() {
+    test1();
+    test2();
+    test3();
+    return 0;
+}
